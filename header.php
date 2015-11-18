3a38230d8c677f061fa130e56dd2371513bedaec
Enzo A. Culiolo <enzo.culiolo@actigraphcorp.com>
2015-11-18 15:23:42 -0600

    Added website files to master branch
    Copied all files to Github.

---
+++ header.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html lang="en">
+<!--<![endif]-->
+
+<head>
+<!-- Title
+    ================================================== -->
+    <title><?php wp_title('|', true, 'right'); ?> <?php bloginfo('name'); ?></title>
+
+<!-- Basic Page Needs
+    ================================================== -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="description" content="This is my personal website to showcase my work, provide services, and get inspired.">
+    <meta name="keywords" content="Enzo Culiolo, Enzo, Culiolo">
+    <meta name="author" content="Enzo A. Culiolo">
+
+<!-- Mobile Specific Metas
+    ================================================== -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<!-- Favicon
+    ================================================== -->
+    <link rel="shortcut icon" href="/wp-content/themes/enzoculiolo1.0/assets/images/ec-favicon-red.ico">
+
+<!-- CSS
+    ================================================== -->
+    <!-- Bootstrap -->
+    <link type="text/css" rel="stylesheet" href="/wp-content/themes/enzoculiolo1.0/assets/bootstrap/css/bootstrap.min.css">
+    <!-- Web Font -->
+    <link href='http://fonts.googleapis.com/css?family=Roboto:100,300,400' rel='stylesheet' type='text/css'>
+    <!-- Plugin CSS -->
+    <link rel="stylesheet" type="text/css" href="/wp-content/themes/enzoculiolo1.0/assets/js-plugin/animation-framework/animate.css" />
+    <!-- Pop Up -->
+    <link rel="stylesheet" type="text/css" href="/wp-content/themes/enzoculiolo1.0/assets/js-plugin/magnific-popup/magnific-popup.css" />
+    <!-- nekoAnim -->
+    <link rel="stylesheet" type="text/css" href="/wp-content/themes/enzoculiolo1.0/assets/js-plugin/appear/nekoAnim.css" />
+    <!-- Owl Carousel -->
+    <link rel="stylesheet" href="/wp-content/themes/enzoculiolo1.0/assets/js-plugin/owl.carousel/owl-carousel/owl.carousel.css">
+    <link rel="stylesheet" href="/wp-content/themes/enzoculiolo1.0/assets/js-plugin/owl.carousel/owl-carousel/owl.transitions.css">
+    <link rel="stylesheet" href="/wp-content/themes/enzoculiolo1.0/assets/js-plugin/owl.carousel/owl-carousel/owl.theme.css">
+    <!-- Icon Fonts -->
+    <link type="text/css" rel="stylesheet" href="/wp-content/themes/enzoculiolo1.0/assets/font-icons/custom-icons/css/custom-icons.css">
+    <!-- Custom CSS -->
+    <link type="text/css" rel="stylesheet" href="/wp-content/themes/enzoculiolo1.0/assets/css/layout.css">
+    <link type="text/css" id="colors" rel="stylesheet" href="/wp-content/themes/enzoculiolo1.0/assets/css/colors.css">
+    <link type="text/css" rel="stylesheet" href="/wp-content/themes/enzoculiolo1.0/assets/css/custom.css">
+    <link type="text/css" rel="stylesheet" href="/wp-content/themes/enzoculiolo1.0/style.css">
+    <!-- Scripts -->
+    <script src="/wp-content/themes/enzoculiolo1.0/assets/js/modernizr-2.6.1.min.js"></script>
+    <!-- Old IE Browser Scripts -->
+    <!--[if lt IE 9]>
+    	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+<!-- Wordpress Head
+    ================================================== -->
+    <?php wp_head(); ?>
+
+<!-- Track Pages in Google Analytics
+    ================================================== -->
+    <?php include_once("includes/analyticstracking.php") ?>
+
+</head>
+
+<body class="activateAppearAnimation">
+
+<!-- Primary Page Layout
+    ================================================== -->
+    <!-- globalWrapper -->
+    <div id="globalWrapper" class="localscroll">
+        <header class="navbar-fixed-top">
+            <!-- pre header -->
+            <div id="preHeader" class="hidden-xs">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-6">
+                            <ul class="quickMenu">
+                            	<li><a href="template-about.html">Home</a></li>
+                                <li><a href="template-about.html">About</a></li>
+                                <li><a href="template-team.html">Portfolio</a></li>
+                                <li><a href="template-contact.html">Contact</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-xs-6">
+                            <div id="contactBloc" class="clearfix">
+                                <!-- social icons -->
+                                <ul class="socialNetwork">
+                                    <li><a href="#" class="tips" title="follow me on Facebook"><i class="icon-facebook-1"></i></a></li>
+                                    <li><a href="#" class="tips" title="follow me on Twitter"><i class="icon-twitter-bird"></i></a></li>
+                                    <li> <a href="#" class="tips" title="follow me on Google+"><i class="icon-gplus-1"></i></a> </li>
+                                    <li> <a href="#" class="tips" title="follow me on Linkedin"><i class="icon-linkedin-1"></i></a> </li>
+                                    <li> <a href="#" class="tips" title="follow me on Dribble"><i class="icon-dribbble"></i></a> </li>
+                                </ul>
+                                <!-- social icons -->
+
+                                <!-- phone -->
+                                <span class="contactPhone"><i class="icon-mobile"></i>850.000.0209</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- / pre header -->
+
+            <!-- header -->
+            <div id="mainHeader" role="banner">
+                <div class="container">
+                    <nav class="navbar navbar-default" role="navigation">
+                        <div class="navbar-header">
+                            <!-- responsive navigation -->
+                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                            	<span class="sr-only">Toggle navigation</span>
+                            	<span class="icon-bar"></span>
+                            	<span class="icon-bar"></span>
+                            	<span class="icon-bar"></span>
+                            </button>
+                            <!-- Logo -->
+                            <a class="navbar-brand" href="/">
+                            	<img src="/wp-content/themes/enzoculiolo1.0/assets/images/ec-logo-red.png" alt="Enzo Culiolo Logo" />
+                            </a>
+                        </div>
+                        <div class="collapse navbar-collapse" id="mainMenu">
+                            <!-- Main navigation -->
+                            <ul class="nav navbar-nav pull-right">
+                                <!-- Links -->
+								<?php wp_nav_menu(array(
+									'menu'            => 'Site Menu',
+									'container'       => false,
+									'items_wrap'      => '%3$s',
+									'link_attr_data'  => array('toggle' => 'hasSubMenu'),
+									'link_attr_class' => 'firstLevel',
+									'walker'          => new main_site_walker_nav_menu()
+								)); ?>
+								<!-- End Links -->
+                            </ul>
+                            <!-- End main navigation -->
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </header>
+        <!-- / header -->
---
+++ templates/page_about.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * Template Name: About
+ */
+
+the_post();
+
+get_header();
+?>
+
+<!-- ======================================= content ======================================= -->
+<main id="content">
+    <header class="page-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    <h1>About</h1>
+                </div>
+                <div class="col-sm-6 hidden-xs">
+                    <ul id="navTrail">
+                        <li><a href="index.html">Home</a></li>
+                        <li id="navTrailLast">About</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- clients -->
+    <section class="pt40 color4">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4" data-nekoanim="fadeInRight" data-nekodelay="10">
+                    <img src="/wp-content/themes/enzoculiolo1.0/assets/images/theme-pics/me.png" alt="Enzo A. Culiolo" class="img-responsive">
+                </div>
+                <div class="col-md-8">
+                    <h2>Enzo A. Culiolo</h2>
+                    <p>
+                        Enzo is an award-winning Graphics Designer with twelve years of professional experience in marketing and advertising with expertise in creative and art direction, graphics design, UI and UX design on website and mobile applications, web design, and app design. Enzo is also a Web Developer with expertise in using Wordpress, Bootstrap, PHP, JavaScript, JQuery, and CSS framework.<br><br>
+                        Enzo is passionate about great design, hard work, and close attention to details.<br><br>
+                        Enzo is currently working as a Graphics Designer at <a href="http://www.actigraphcorp.com">ActiGraph, LLC</a>, in Pensacola, Florida. His areas of expertise include creative direction, art direction, image branding, tradeshows, advertising, collaterals, app design, software design, and website design and development.<br><br>
+                        Enzo has worked as an Art Director for award-winning publication <a href="http://www.lifesabeachllc.com/">Life's A Beach, LLC</a>, in Destin, Florida. His areas of expertise included art direction for magazine, advertising, collaterals, and website design and development.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- / clients -->
+    <!-- skills -->
+    <section id="services" class="pt40 pb40">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4" data-nekoanim="fadeInUp" data-nekodelay="300">
+                    <h2>What I can offer</h2>
+                    <p>I offer both conventional and unconventional creative services to suit the needs of any client.</p>
+                    <br>
+                    <h2>Mission</h2>
+                    <p>As an explorer of new, clean, and fresh designs, I thrive on new discoveries, hard work, and attention to details. I bring this same passion into every project I execute, making sure I provide the best possible end result for my clients.</p>
+                    <br>
+                    <h2>Goals</h2>
+                    <p>I'm always looking to make the next leap forward. If your company needs a logo, a website, an ad campaign, photography, print or even marketing, I can certainly help achieve your goals.</p>
+                    <br>
+                    <h2>Favorite Quote</h2>
+                    <p>
+                        <i>“Advertising is the ability to sense, interpret... to put the very heart throbs of a business into type, paper and ink.”</i> - Leo Burnett
+                    </p>
+                </div>
+                <div class="col-sm-4" data-nekoanim="fadeInUp" data-nekodelay="400">
+                    <h2>Accolades</h2>
+                    <p>6 Gold ADDYs including “Best of Show” as a student and one Silver ADDY as a professional.</p>
+                    <br>
+                    <h2>Clubs and Memberships</h2>
+                    <p><a href="http://www.westerngate.toastmastersclubs.org">Western Gate</a> Toastmaster Club, in Pensacola, Florida.</p>
+                    <br>
+                    <h2>Born</h2>
+                    <p>February 9, 1980, in Panama City, Panama.</p>
+                    <br>
+                    <h2>Education</h2>
+                    <p>Enzo moved to the United States in 1998 at the age of eighteen, after graduating high school. He attended and graduated college with two A.A.S. degrees in Graphics Design Technology and Multimedia from <a href="http://www.nwfsc.edu/">Northwest Florida State College</a>, former Okaloosa-Walton Community College in May of 2003. He also attended and graduated with a Bachelor's of Communication Arts in Advertising from <a href="http://www.uwf.edu/">University of West Florida</a> in December of 2012.</p>
+                    <br>
+                    <h2>Personal</h2>
+                    <p>Enzo lives with his wife and own two condos in Destin, Florida and have a little chihuahua named Sophie. He enjoys playing and watching sports, especially soccer, college football, and basketball. His favorite pass times are watching movies and being an amateur astronmer.</p>
+                </div>
+                <div class="col-sm-4" data-nekoanim="fadeInUp" data-nekodelay="200">
+                    <h2>Skills</h2>
+                    <h4>Creative Direction</h4>
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;"> <span>100%</span> </div>
+                    </div>
+                    <h4>Art Direction</h4>
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;"> <span>100%</span> </div>
+                    </div>
+                    <h4>Marketing</h4>
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100" style="width: 95%;"> <span>95%</span> </div>
+                    </div>
+                    <h4>Branding</h4>
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100" style="width: 95%;"> <span>95%</span> </div>
+                    </div>
+                    <h4>Web Development</h4>
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100" style="width:85%;"> <span>85%</span> </div>
+                    </div>
+                    <h4>User Interface/Experience</h4>
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width: 90%;"> <span>90%</span> </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- / skills -->
+</main>
+<!-- / content -->
+
+<?php get_footer(); ?>
